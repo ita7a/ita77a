508a9355ddcd39c091434df27edfed456990e388
ita7a <57022144+ita7a@users.noreply.github.com>
2020-12-01 11:16:36 +0300

    Create MuHanad.js

---
+++ MuHanad.js
@@ -0,0 +1,26 @@
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample">- ¦¦ - شات التحليه - ¦¦ -</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '3px solid #FFFFFF',borderLeft: '3px solid #E0E0E0',borderTop: '1px solid #E0E0E0',borderBottom: '1px solid #FFFFFF',paddingTop: '3px',borderRadius: '5px'});
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<style>
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(112, 112, 113);border-radius: 75px 75px 75px 75px;}
+.fr.co {
+    transform: rotate(0deg);
+    height: 10px;
+    width: 16px!important;
+    margin-top: 4px;
+</style>`).insertBefore('body');
+$(`<img tabindex="0" class="al120 fl " style="margin-left: -4px; padding:5px; width:40px;margin-top: -4px;" src="/sico/1558730372080.png" data-original-title="" title="">`).insertAfter('img.fl.nosel.emobox');
+$(".al120").click(function(){
+alert("عذرا لا تملك الصلاحية للدخول  ");
+});
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><input type="Mu" name="FirstName" value="FM اذاعة">
+<audio class="wp-audio-shortcode" id="audio-236-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="http://curiosity.shoutca.st:6035/ ؛"><a href="</a></audio>
+<center></center></center>
+</div>`);
+$('#d0').append(`<label title="الاذاعه" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
+data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">FM</label>`);
+$("#d0 .fa-gear").html('&nbsp;').css('width','36px');
+$(`<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"><font style="background-color: #‏B7B6BA;border-radius: 16px 16px 16px 15px;font-family: 'jazeera',FontAwesome;padding: 5px 10px 5px 10px;margin: 10px 13px 10px 13px;"><img src="/sico/1592924319238.gif" style="width: 100%;transform:matrix(1, 0, 0, 1, 8, 0);"></a>
+<div style=" width: 100%; " id="ita7a" class="btn btn-primary btn/marquee></font></b></td>`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success'); 
+$(`<img src="/sico/1571434199503.gif" style="width:100%;margin-top: 0px;margin-bottom: 0px;" >
+<div style=" width: 100%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">اداره الموقع</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  &nbsp;<i class="fa fa-paper-plane">  &nbsp;</i>للشكاوي فقط<p><i class="fa fa-comment"></i> <i class="fa fa-share"></i></p><i class="fa fa-comment"></i> <i class="fa fa-share">Snap:ita7a.com</i> </p>  <i class="fa fa fa-comment"></i> <i class="fa fa-share">     </i>          </div></div><center></div></div><center><center><div><marquee direction="right" 
+`).insertBefore('.nav-tabs')
